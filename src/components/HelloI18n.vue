f81a43852cfbeec8c4381a4261989526bf47299e
Ibrahim.H <bitsnaps@yahoo.fr>
2022-10-12 17:13:28 +0100

    number formatting

--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+
       <p>{{ $t("hello", { name}) }}</p>
       <p>{{ $t("goodbye") }}</p>
 
@@ -9,6 +10,7 @@
       <p>{{ $t('car', 1)}}</p>
       <p>{{ $t('car', 2)}}</p>
 
+    <h2>Pluralization</h2>
       <!-- use "count" or "n" to show the number of item -->
       <p>{{ $t('person', 0)}}</p>
       <p>{{ $t('person', 1)}}</p>
@@ -20,10 +22,25 @@
       <p>{{ $t('apple', 2)}}</p>
       <p>{{ $t('apple', 3)}}</p>
 
+      <h2>Date Formatting</h2>
+      <p>Short: {{ $d(new Date(), 'short')}}</p>
+      <p>Long: {{ $d(new Date(), 'long')}}</p>
+      <p>Fr: {{ $d(new Date(), 'short', 'fr')}}</p>
+      <!-- <p>Fr: {{ $d(new Date(), 'short', 'fr-FR')}}</p> -->
+
+      <h2>Number Formatting</h2>
+      <p>{{ $n(10000, 'currency') }}</p>
+
       <button @click="setLocale('en')" class="button">English</button>
       <button @click="setLocale('es')" class="button">Spanish</button>
       <button @click="setLocale('ar')" class="button">عربي</button>
       <button @click="setLocale('fr')" class="button">French</button>      
+      <br>
+      <h2>Localization by Countries:</h2>
+      <p>{{ product.name }}: {{ product.price}}, with {{ product.stock }} left in stock.</p>
+      <button @click="setLocaleCountry('en-US')" class="button">USA</button>
+      <button @click="setLocaleCountry('en-GB')" class="button">UK</button>
+      <button @click="setLocaleCountry('es-ES')" class="button">Spain</button>
 
     <div class="locale-changer">
         <p>Your browser uses <code>{{ language }}</code> you can switch to your prefered language:</p>
@@ -37,7 +54,25 @@
     </div>
   </template>
   
-  <script lang="ts">
+  <script>
+
+  const products = {
+    'en-US': {
+        name: 'Red jeans',
+        stock: 2,
+        price: 109
+    },
+    'en-GB': {
+        name: 'Red jeans',
+        stock: 4,
+        price: 89
+    },
+    'es-ES': {
+        name: 'Vaqueros Rojos',
+        stock: 9,
+        price: 99
+    }
+  }
 
   export default {
     name: "HelloI18n",
@@ -47,12 +82,14 @@
         }
     },
     methods: {
-      setLocale(locale: string) {
+      setLocale(locale /*: string*/) {
         console.log('changed to: ', locale);
-        
         this.$i18n.locale = locale
         // localStorage has no expiration time
         localStorage.setItem('locale', locale)
+      },
+      setLocaleCountry(locale /*: string*/) {
+        this.$i18n.locale = locale
       }
     }, // methods
     computed: {
@@ -61,6 +98,9 @@
         },
         languages() {
             return window.navigator.languages
+        },
+        product(){
+            return products[this.$i18n.locale.indexOf('-') > -1? this.$i18n.locale: 'en-US']
         }
     },
     created() {
@@ -69,6 +109,8 @@
             this.$i18n.locale = locale
         } else if(navigator.language){
             this.$i18n.locale = navigator.language.slice(0,2)
+            // unless you want to use the secondary language specific region (e.g. en-GB...)
+            // this.$i18n.locale = navigator.language
         }
     }
   };
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createI18n } from 'vue-i18n'
+import { createI18n, IntlDateTimeFormats } from 'vue-i18n'
 import en from './locales/en.json'
 import es from './locales/es.json'
 import ar from './locales/ar.json'
@@ -29,13 +29,91 @@ const messages = {
   }
 */
 
+// Format uses ISO codes
+const datetimeFormats: IntlDateTimeFormats = {
+  // 'en-US': {
+  'en': { // short format only
+    short: {
+      // year: 'numeric', month: 'short', day: 'numeric'
+      year: '2-digit', month: 'short', day: 'numeric'
+    },
+    // Optional
+    long: {
+      year: 'numeric', month: 'short', day: 'numeric',
+      weekday: 'short', hour: 'numeric', minute: 'numeric'
+    }
+  },
+  // 'fr-FR': {
+  'fr': {
+    short: {
+      // year: 'numeric', month: 'short', day: 'numeric'
+      year: 'numeric', month: '2-digit', day: 'numeric'
+    },
+    long: {
+      year: 'numeric', month: 'short', day: 'numeric',
+      weekday: 'short', hour: 'numeric', minute: 'numeric', hour12: true
+    }
+  }
+}
+
+const numberFormats: any /* Intl.NumberFormat */ = {
+  'en': {
+    currency: {
+      style: 'currency', currency: 'USD', notation: 'standard'
+    },
+    decimal: {
+      style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2
+    },
+    percent: {
+      style: 'percent', useGrouping: false
+    }
+  },
+  'en-GB': {
+    currency: {
+      style: 'currency', currency: 'GBP', notation: 'standard'
+    },
+    decimal: {
+      style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2
+    },
+    percent: {
+      style: 'percent', useGrouping: false
+    }
+  },
+  'es-ES': {
+    currency: {
+      style: 'currency', currency: 'EUR', useGrouping: true, currencyDisplay: 'symbol' // or 'code' or 'name'
+    },
+    decimal: {
+      style: 'decimal', minimumSignificantDigits: 3, maximumSignificantDigits: 5
+    },
+    percent: {
+      style: 'percent', useGrouping: false
+    }
+  },
+  'fr': {
+    currency: {
+      style: 'currency', currency: 'EUR', useGrouping: true, currencyDisplay: 'symbol'
+    },
+    decimal: {
+      style: 'decimal', minimumSignificantDigits: 3, maximumSignificantDigits: 5
+    },
+    percent: {
+      style: 'percent', useGrouping: false
+    }
+  }
+}
+
 // 2. Create i18n instance with options
 const i18n = createI18n({
-    locale: 'es', // set locale
+    // locale: 'es', // set locale
+    // locale: 'en-US',
+    locale: 'en',
     fallbackLocale: 'en', // set fallback locale
     messages: { en, es, ar, fr }, // set locale messages
     // If you need to specify other options, you can set other options
     // ...
+    datetimeFormats,
+    numberFormats
   })
 
 createApp(App)
