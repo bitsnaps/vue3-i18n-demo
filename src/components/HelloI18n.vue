39a28933d623f9d941fb940d76120706752dc3d5
Ibrahim.H <bitsnaps@yahoo.fr>
2022-10-12 17:42:54 +0100

    link in text

--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -37,10 +37,43 @@
       <button @click="setLocale('fr')" class="button">French</button>      
       <br>
       <h2>Localization by Countries:</h2>
+      <p>{{ $n(0.99123, 'percent', { minimumFractionDigits: 2 }) }}</p>
+
       <p>{{ product.name }}: {{ product.price}}, with {{ product.stock }} left in stock.</p>
       <button @click="setLocaleCountry('en-US')" class="button">USA</button>
       <button @click="setLocaleCountry('en-GB')" class="button">UK</button>
-      <button @click="setLocaleCountry('es-ES')" class="button">Spain</button>
+      <button @click="setLocaleCountry('es')" class="button">Spain</button>
+
+      <h2>I18n Component</h2>
+      <i18n-n :value="200"></i18n-n>
+      <br>
+      <i18n-n :value="200" format="currency"></i18n-n>
+      <br>
+      <!-- separate currency from number using slots-->
+      <i18n-n :value="200" format="currency">
+        <template v-slot:currency="slotProps">
+            <span style="color: red">{{ slotProps.currency}}</span>
+        </template>
+        <template v-slot:integer="slotProps">
+            <span style="font-weight: bold;">{{ slotProps.integer}}</span>
+        </template>
+        <template v-slot:decimal="slotProps">
+            <span>{{ slotProps.decimal}}</span>
+        </template>
+        <template v-slot:fraction="slotProps">
+            <span>{{ slotProps.fraction}}</span>
+        </template>
+    </i18n-n>
+
+    <h2>Link in text</h2>
+    <i18n-t keypath="terms" tag="p">
+        <template v-slot:termsURL>
+            <!-- You use a button to open a Modal -->
+            <a href="/terms">{{ $t("termsLink")}}</a>
+        </template>
+    </i18n-t>
+
+
 
     <div class="locale-changer">
         <p>Your browser uses <code>{{ language }}</code> you can switch to your prefered language:</p>
@@ -67,7 +100,7 @@
         stock: 4,
         price: 89
     },
-    'es-ES': {
+    'es': {
         name: 'Vaqueros Rojos',
         stock: 9,
         price: 99
